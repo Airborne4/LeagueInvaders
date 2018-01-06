cc6dc3a047d24ffdb04c2975cf21c75b8e140e76
League <league@WTS8.attlocal.net>
2018-01-05 16:58:44 -0800

    b
    bn

--- src/Projectile.java
+++ src/Projectile.java
@@ -3,17 +3,23 @@ import java.awt.Graphics;
 
 public class Projectile extends GameObject {
 	int speed;
+
 	public Projectile(int x, int y, int width, int height) {
 		super();
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
-		speed=10;
+		speed = 10;
 	}
-	void update() {
 
+	void update() {
+		y -=speed;
+		if (y<0) {
+			isAlive=false;
+		}
 	}
+
 	void draw(Graphics g) {
 		g.setColor(Color.RED);
 		g.drawRect(x, y, width, height); // 10-5
